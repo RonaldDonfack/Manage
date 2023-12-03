66adbeb8d8709b83e64995e69d0c28ba8405f183
RonaldDonfack <donfackronald13@gmail.com>
2023-12-03 09:36:44 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens : {
+      sm: '450px',
+      md: '768px',
+      lg: '978px',
+      xl: '1232px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
